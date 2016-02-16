b07edd65bb7a68d7de872a27f55d5dd8cefc3269
Boris Marinov <Boris_Marinov@epam.com>
2016-02-16 11:27:23 +0200

    chapter 2

--- 2-parse/main.hs
+++ 2-parse/main.hs
@@ -3,6 +3,7 @@ import System.Environment
 import Control.Monad
 
 data LispVal = Atom String
+             | Nil
              | List [LispVal]
              | DottedList [LispVal] LispVal
              | Number Integer
@@ -15,19 +16,44 @@ parseExpr = parseChar
          <|> parseAtom
          <|> parseString
          <|> parseNumber
+         <|> parseQuoted
          <|> do char '('
-                x <- try parseList <|> parseDottedList
+                --x <- try parseList <|> parseDottedList
+                x <- parseAnyList
                 char ')'
                 return x
+
+
+trySpaces :: Parser ()
+trySpaces = spaces <|> return ()
+
+--3. Instead of using the try combinator, left-factor the grammar so that the common subsequence is its own parser. 
+
+parseAnyList :: Parser LispVal
+parseAnyList  = do 
+    list <- sepBy parseExpr space
+    trySpaces
+    tail <- (char '.' >> spaces >> parseExpr) <|> return Nil
+    return $ case tail of 
+        Nil -> List list
+        otherwise -> DottedList list tail
 
 parseList :: Parser LispVal
 parseList = sepBy parseExpr space >>= \a -> return $ List a
 
+  
+
 parseDottedList :: Parser LispVal
 parseDottedList = do
-    head <- endBy parseExpr spaces
+    head <- sepBy parseExpr spaces
     tail <- char '.' >> spaces >> parseExpr
-    return $ DottedList head tail
+    return $ DottedList head tail 
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+    char '\''
+    x <- parseExpr
+    return $ List [Atom "quote", x]
 
 escapedChars :: Parser Char
 escapedChars = do 
@@ -35,6 +61,8 @@ escapedChars = do
               char '\\'
               x <- oneOf "\\\""
               return x
+
+-- 2. Change parseString so that \" gives a literal quote character instead of terminating the string. 
 
 chars :: Parser Char
 chars = escapedChars <|> noneOf "\""
@@ -44,6 +72,7 @@ parseChar  = do
               string "\\#"
               ch <- chars
               return $ Character ch
+
 
 parseString :: Parser LispVal
 parseString = do
@@ -74,13 +103,19 @@ readExpr input = case parse parseExpr "lisp" input of
     Left err -> "No match: " ++ show err
     Right v -> "Found value" ++ show v
 
+
 parseNumber :: Parser LispVal
 --parseNumber = liftM (Number . read) $ many1 digit
+
+--1. Rewrite parseNumber, without liftM, using:
+
+-- 1.explicit sequencing with the >>= operator
 parseNumber  = many1 digit >>= \val -> return $ Number (read val)
 
 --TODO find why this does not work
 --parseNumber str = many1 digit $ str >>= \val -> return $ Number (read val)
 
+-- 2.do-notation
 --parseNumber = do 
 --                val <- many1 digit 
 --                return $ Number (read val)
@@ -89,4 +124,3 @@ parseNumber  = many1 digit >>= \val -> return $ Number (read val)
 main = do 
   val <- getLine
   putStrLn $ readExpr val
-  
