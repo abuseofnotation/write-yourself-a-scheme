2ae9b9855015cd05ceaf3b555ccc7370f3482bd8
Boris Marinov <Boris_Marinov@epam.com>
2016-02-17 15:27:04 +0200

    Added eval function

--- Eval/Eval.hs
+++ Eval/Eval.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts, MultiWayIf #-}
 module Eval.Eval where
 
 import Parse.Parse
@@ -8,4 +9,19 @@ eval :: LispVal -> LispVal
 eval val@(String _) = val
 eval val@(Number _) = val
 eval val@(Bool _) = val
+--list literal
 eval (List [Atom "quote", val]) = val
+--function application
+eval (List ((Atom f):args)) = apply f (map eval args)
+
+
+apply :: String -> [LispVal] -> LispVal
+
+apply "+" list = Number $ sum (toNumbers list)
+
+toNumber :: LispVal -> Integer
+toNumber (Number x) = x
+
+toNumbers :: [LispVal] -> [Integer]
+toNumbers = fmap toNumber
+
