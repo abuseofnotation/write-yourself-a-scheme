8eb3eb7dc852ccec1f07dff7502210f59556e435
Boris Marinov <Boris_Marinov@epam.com>
2016-02-19 15:05:53 +0200

    Adding primitive functions

--- Eval/Eval.hs
+++ Eval/Eval.hs
@@ -2,6 +2,7 @@
 module Eval.Eval where
 
 import Parse.Parse
+import Eval.Unpack
 import Data.List
 import Data.Either
 
@@ -17,6 +18,8 @@ eval (List [Atom "quote", val]) = Right val
 --function application
 eval (List ((Atom f):args)) = (evalArgs args) >>= apply f 
 
+
+eval badForm = Left $ "Unrecognized form" ++ show badForm
 --list
 evalArgs :: [LispVal] -> Either String LispVal
 evalArgs args = case find isLeft evaluated of
@@ -26,8 +29,57 @@ evalArgs args = case find isLeft evaluated of
 
 --A list of functions
 functions :: [(String, [LispVal] -> LispVal)]
-functions = [
-  ("+", \args -> Number $ sum (toNumbers args))]
+functions = [("+", numberToNumber (+)),
+              ("-", numberToNumber (-)),
+              ("*", numberToNumber (*)),
+              ("/", numberToNumber div),
+              ("mod", numberToNumber mod),
+              ("quotient", numberToNumber quot),
+              ("remainder", numberToNumber rem),
+              ("=", numberToBoolean (==)),
+              ("<", numberToBoolean (<)),
+              (">", numberToBoolean (>)),
+              ("/=", numberToBoolean (/=)),
+              (">=", numberToBoolean (>=)),
+              ("<=", numberToBoolean (<=)),
+              ("&&", booleanToBoolean (&&)),
+              ("||", booleanToBoolean (||)),
+              ("string=?", stringToBoolean (==)),
+              ("string<?", stringToBoolean (<)),
+              ("string>?", stringToBoolean (>)),
+              ("string<=?", stringToBoolean (<=)),
+              ("string>=?", stringToBoolean (>=)),
+              ("car", car),
+              ("cdr", cdr)]
+              --("cons", cons)]
+
+car :: [LispVal] -> LispVal
+car [List (x:xs)] = x
+car [(DottedList (x:xs) tail)] = x
+
+cdr [List (x:xs)] = List xs
+cdr [(DottedList (x:[]) tail)] = tail
+cdr [(DottedList (x:xs) tail)] = DottedList xs tail
+
+--Converts a Haskell function to Lisp function
+toLispFunction unpack pack func = \list -> pack $ func (map unpack list)
+
+numberToNumber :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numberToNumber = toLispFunction unpackNum Number . foldl1
+
+numberToBoolean = toLispFunction unpackNum Bool . applyBoolean
+
+booleanToBoolean = toLispFunction unpackBoolean Bool . applyBoolean
+
+stringToBoolean = toLispFunction unpackString Bool . applyBoolean
+
+applyBoolean _ [] = False
+applyBoolean _ [val] = False
+applyBoolean op (val1:val2:xs) = if (op val1 val2)
+                                    then if xs == []
+                                        then True
+                                        else applyBoolean op (val2:xs)
+                                    else False
 
 --Evaluating function application
 apply :: String -> LispVal -> Either String LispVal
